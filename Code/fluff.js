3006acef9ceed7b73ebb5eb2cd32ed557cf6f368
Visionist <escaperecovery@gmail.com>
2019-06-13 19:00:19 -0700

    Update fluff.js

--- Code/fluff.js
+++ Code/fluff.js
@@ -2,7 +2,7 @@
 function movePlayer(event){
    var x = event.clientX;
    var y = event.clientY;
-   document.getElementById("player").style="transform:translate(-50%,-50%);position:relative;left:"+(x)+"px;top:"+(y)+"px;";
+   document.getElementById("player").style="transform:translate(-50%,-50%);position:absolute;left:"+(x)+"px;top:"+(y)+"px;";
 }
 
 
