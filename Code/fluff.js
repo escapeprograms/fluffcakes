8d89d56b28eadc1fe7c6d16a67d6faacd9a0b22c
Visionist <escaperecovery@gmail.com>
2019-06-14 11:35:56 -0700

    Update fluff.js

--- Code/fluff.js
+++ Code/fluff.js
@@ -2,7 +2,7 @@
 function movePlayer(event){
    var x = event.clientX;
    var y = event.clientY;
-   document.getElementById("player").style="transform:translate(-75%,-150%);position:absolute;left:"+(x)+"px;top:"+(y)+"px;";
+   document.getElementById("player").style="transform:translate(-50%,-50%);position:absolute;left:"+(x)+"px;top:"+(y)+"px;";
 }
 
 function Obstacle(pos,vel,acc,img,dmg){
