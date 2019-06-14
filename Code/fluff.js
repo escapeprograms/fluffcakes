9dadaabccfd9cc7f45615830310b1c71daf5c0f1
Visionist <escaperecovery@gmail.com>
2019-06-14 13:41:54 -0700

    Update fluff.js

--- Code/fluff.js
+++ Code/fluff.js
@@ -5,12 +5,22 @@ function movePlayer(event){
    document.getElementById("player").style="transform:translate(-50%,-50%);position:absolute;left:"+(x)+"px;top:"+(y)+"px;";
 }
 
-function Obstacle(pos,vel,acc,img,dmg){
-   this.pos=pos;
-   this.vel=vel;
-   this.acc=acc;
-   this.img=img;
-   this.dmg=dmg;
+function Obstacle(id,pos,vel,acc,img,dmg,size){
+   this.id=id;//int
+   this.pos=pos;//[x,y]
+   this.vel=vel;//[dx,dy]
+   this.acc=acc;//[d2x,d2y]
+   this.img=img;//file.png
+   this.dmg=dmg;//int
+   this.size=size;//[width,height]
    this.active=true;
+   
+   this.draw = function(){
+      document.getElementsByClassName("obs")[this.id].innerHTML="<img src = '"+this.img+"'>";
+   }
 }
 
+
+
+
+var obstacles = [new Obstacle(0,[0,0],[0,0],[0,0],"/fluffcakes/Images/Misc/chest.png",0,[100,100])];
