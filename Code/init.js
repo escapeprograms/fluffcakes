4b22edcd828032f38a67f82620f314d74f47fffa
Visionist <escaperecovery@gmail.com>
2019-06-13 18:43:34 -0700

    Update init.js

--- Code/init.js
+++ Code/init.js
@@ -2,7 +2,7 @@ function init(){
   //load map locations
   for (var i = 0; i < locations.length; i++){visit(i,true);}
   //starter page
-  page(0);
+  page(6);
   //map
   resizeMap();
   updateMap();
