2624e26f0b3602c803e2e30d72547b1ab2fcfb7e
Visionist <escaperecovery@gmail.com>
2019-06-13 18:54:02 -0700

    Update fluff.js

--- Code/fluff.js
+++ Code/fluff.js
@@ -1,1 +1,9 @@
 //testing
+function movePlayer(event){
+   var x = event.clientX;
+   var y = event.clientY;
+   document.getElementById("player").style="transform:translate(-50%,-50%);position:relative;left:"+(x)+"px;top:"+(y)+"px;";
+}
+
+
+
