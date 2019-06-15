28ddba8a73553c50a9f016f54cff694b7b00466d
Visionist <escaperecovery@gmail.com>
2019-06-15 16:46:47 -0700

    Update fluff.js

--- Code/fluff.js
+++ Code/fluff.js
@@ -50,21 +50,17 @@ function Obstacle(id,pos,vel,acc,img,dmg,size,s){
 function bounce(id){
      if (this.t>1){
         if (obstacles[id].pos[0]>=100-obstacles[id].vel[0]||obstacles[id].pos[0]<=obstacles[id].vel[0]){
-           obstacles[id].vel[0]*=-1;
+           obstacles[id].vel[0]*=0;
           // alert(obstacles[id].vel);
         }
         if (obstacles[id].pos[1]>=100-obstacles[id].vel[1]||obstacles[id].pos[1]<=obstacles[id].vel[1]){
-           obstacles[id].vel[1]*=-1;
+           obstacles[id].vel[1]*=0;
           // alert(obstacles[id].vel);
         }
      }
 }
 
-var obstacles = [new Obstacle(0,[0,0],[1,1],[0,0],"/fluffcakes/Images/Misc/chest.png",0,[5,5],(id)=>{bounce(id);})];
-for (var i = 0; i < obstacles.length; i++){
-   obstacles[i].create();
-   obstacles[i].draw();
-}
+var obstacles = [];
 //update
 setInterval(()=>{
    for (var i = 0; i < obstacles.length; i++){
